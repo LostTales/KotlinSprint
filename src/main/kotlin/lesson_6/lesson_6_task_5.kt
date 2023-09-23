c4d924ad16c531b2d2659939b88d1d4e96ae5e48
Vadim <fullmetallboss@gmail.com>
2023-09-23 18:04:27 +0300

    KS-6-5

--- src/main/kotlin/lesson_6/lesson_6_task_5.kt
+++ src/main/kotlin/lesson_6/lesson_6_task_5.kt
@@ -2,28 +2,23 @@ package lesson_6
 
 fun main() {
 
-    var number1 = (1..9).random()
-    var number2 = (1..9).random()
-
-    println("Ведите сумму двух чисел: $number1 + $number2")
-    var userAnswer = readln().toInt()
-
-    var sumOfNumbers = number1 + number2
-
     var numberOfTrials = 3
 
     while (numberOfTrials > 0) {
+
+        var number1 = (1..9).random()
+        var number2 = (1..9).random()
+        println("Ведите сумму двух чисел: $number1 + $number2")
+        var userAnswer = readln().toInt()
+        var sumOfNumbers = number1 + number2
+
         if (userAnswer == sumOfNumbers) {
             println("Добро пожаловать!")
             break
         } else {
             println("Неправильный ответ. Повторно пройдите тест.")
             numberOfTrials--
-            number1 = (1..9).random()
-            number2 = (1..9).random()
-            println("Ведите сумму двух чисел: $number1 + $number2")
-            userAnswer = readln().toInt()
-            sumOfNumbers = number1 + number2
+
         }
         if (numberOfTrials == 0) {
             println("Доступ запрещён.")
