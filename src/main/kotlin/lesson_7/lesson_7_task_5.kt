1f447daa89d18bf68326fb077ea3a42bb8a4c2c7
Vadim <fullmetallboss@gmail.com>
2023-09-24 17:51:03 +0300

    KS-7-5

---
+++ src/main/kotlin/lesson_7/lesson_7_task_5.kt
@@ -0,0 +1,15 @@
+package lesson_7
+
+fun main() {
+
+    val passwordData = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+    val passwordLength = readln().toInt()
+    var password:String = ""
+
+    for (i in 1..passwordLength) {
+        password += passwordData.random()
+    }
+
+    println(password)
+
+}
