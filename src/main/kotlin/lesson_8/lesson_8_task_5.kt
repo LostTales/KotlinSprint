affa4bf5f2f79332a9f38fd3d4fbc0ea62ee9501
Vadim <fullmetallboss@gmail.com>
2023-09-26 14:35:06 +0300

    KS-8-5 moved to lambda

--- src/main/kotlin/lesson_8/lesson_8_task_5.kt
+++ src/main/kotlin/lesson_8/lesson_8_task_5.kt
@@ -3,13 +3,11 @@ package lesson_8
 fun main() {
 
     println("Введите кол-во ингредиентов:")
-    val numberOfIngredients = Array(readln().toInt()) { "" }
-
-    var number = 0
-
-    for (i in 0 until numberOfIngredients.size) {
-        println("Введите название ${++number}-го ингредиента:")
-        numberOfIngredients[i] = readln()
+    var counter = 0
+    val number = readln().toInt()
+    val numberOfIngredients = Array(number) {
+        println("Введите название ${++counter}-го ингредиента:")
+        readln()
     }
 
     println("Список ингредиентов:")
