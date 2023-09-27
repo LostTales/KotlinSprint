8686b4a5b2f18ba177ced7ad5710e9a38ca515fb
Vadim <fullmetallboss@gmail.com>
2023-09-27 13:25:35 +0300

    KS-9-2

---
+++ src/main/kotlin/lesson_9/lesson_9_task_2.kt
@@ -0,0 +1,31 @@
+package lesson_9
+
+fun checklist(content: MutableList<String>) {
+    for (i in content) {
+        println("[$i]")
+    }
+}
+
+fun main() {
+
+    val tea = mutableListOf("вода", "чай", "сахар")
+
+    println("В рецепте есть базовые ингредиенты:")
+    checklist(tea)
+
+    println("Желаете добавить ещё?")
+    val userAnswer = readln()
+
+    if (userAnswer == ANSWER) {
+        println("Какой ингредиент вы хотите добавить?")
+        tea.add(readln())
+    } else {
+        return
+    }
+
+    println("Теперь в рецепте есть следующие ингредиенты:")
+    checklist(tea)
+
+}
+
+const val ANSWER = "да"
