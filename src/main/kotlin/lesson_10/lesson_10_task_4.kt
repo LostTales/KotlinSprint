a7343c4a163147ad2361552bdcd6299a153f0856
Vadim <fullmetallboss@gmail.com>
2023-09-30 21:12:26 +0300

    KS-10-4

---
+++ src/main/kotlin/lesson_10/lesson_10_task_4.kt
@@ -0,0 +1,56 @@
+package lesson_10
+
+fun main() {
+
+    var manScore = 0
+    var computerScore = 0
+    var numberOfGame = 0
+
+    do {
+        ++numberOfGame
+
+        println("Ход игрока.")
+        val manPoints = rollTheDice()
+        println("Всего очков: $manPoints")
+
+        println("Ход компьютера.")
+        val computerPoints = rollTheDice()
+        println("Всего очков: $computerPoints")
+
+        if (manPoints > computerPoints) {
+            println("Победило человечество")
+            ++manScore
+        } else if (manPoints == computerPoints) {
+            println("Ничья")
+        } else {
+            println("Победила машина")
+            ++computerScore
+        }
+
+        println("Хотите бросить кости ещё раз? Введите Да или Нет")
+        val userAnswer = readln()
+    } while (repeatGame(userAnswer))
+
+    println("Игрок выиграл $manScore партий из $numberOfGame")
+
+}
+
+const val ANSWER = "да"
+
+fun rollTheDice(): Int {
+
+    val firstRoll = (1..6).random()
+    println("Первый бросок, выпало: $firstRoll")
+
+    val secondRoll = (1..6).random()
+    println("Второй бросок, выпало: $secondRoll")
+
+    return firstRoll + secondRoll
+
+}
+
+fun repeatGame(userAnswer: String): Boolean {
+
+    return userAnswer.lowercase() == ANSWER
+
+}
