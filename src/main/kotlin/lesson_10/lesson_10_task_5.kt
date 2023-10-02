35ad250bc63d31b5e057a4af8880296fe88914fe
Vadim <fullmetallboss@gmail.com>
2023-10-02 16:46:26 +0300

    KS-10-5 Без переноса всей регистрацию в один метод return Boolean

--- src/main/kotlin/lesson_10/lesson_10_task_5.kt
+++ src/main/kotlin/lesson_10/lesson_10_task_5.kt
@@ -3,62 +3,44 @@ package lesson_10
 
 fun main() {
 
-    println("Введите логин:")
-    val userLoginRegistration = readln()
-    val userRegistration = findLength(userLoginRegistration)
+    println("Регистрация:")
 
-    if (userRegistration >= 4) {
-        println("Задайте длину пароля:")
-    } else {
-        println("Логин недостаточно длинный.")
-        return
-    }
+    val userLoginRegistration = registerLogin()
 
-    val userPasswordRegistration = generatePassword(readln().toInt())
-    println("Пароль:\n$userPasswordRegistration")
+    val userPasswordRegistration = registerPassword()
 
     println("Авторизация:")
 
+    perfomAuthorization(userLoginRegistration, userPasswordRegistration)
+
+}
+
+fun registerLogin(): String {
+
     println(LOGIN_REQUEST_1)
-    val userLogin = readln()
-
-    val loginVerification = if (userLogin == userLoginRegistration) {
-        PASSWORD_REQUEST_1
-    } else {
-        println("Пройдите регистрацию.")
-        return
+    var userLoginRegistration = readln()
+    while (!findLength(userLoginRegistration)) {
+        println("Логин недостаточно длинный.\n$LOGIN_REQUEST_1")
+        userLoginRegistration = readln()
     }
-    println(loginVerification)
-
-    do {
-        val userPassword = readln()
-        authorization(userPassword, userPasswordRegistration)
-    } while (userPassword != userPasswordRegistration)
-
-    do {
-        val code = (1000..9999).random()
-        println("Ваш код авторизации: $code")
-        println("Введите код:")
-        val userAnswer = readln().toInt()
-        if (userAnswer != code) println("Неверный код.")
-    } while (userAnswer != code)
-
-    println("Добро пожаловать!")
+    return userLoginRegistration
 
 }
 
-const val LOGIN_REQUEST_1 = "Введите логин:"
-const val PASSWORD_REQUEST_1 = "Введите пароль:"
+fun registerPassword(): String {
 
-fun findLength(login: String): Int {
-
-    return login.length
+    println("Задайте длину пароля:")
+    val userPasswordRegistration = generatePassword(readln().toInt())
+    println("Пароль:\n$userPasswordRegistration")
+    return userPasswordRegistration
 
 }
+
+fun findLength(login: String) = login.length >= LOGIN_LENGHT
 
 fun generatePassword(passwordLength: Int): String {
 
-    val specialChars = listOf('!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ' ')
+    val specialChars = ' '..'/'
     val digits = '0'..'9'
     var password: String = ""
 
@@ -80,4 +62,35 @@ fun authorization(userPassword: String, userPasswordRegistration: String) {
     } else {
         println("Неверный пароль\n${PASSWORD_REQUEST_1}")
     }
+
 }
+
+fun perfomAuthorization(userLoginRegistration: String, userPasswordRegistration: String) {
+
+    do {
+        println(LOGIN_REQUEST_1)
+        val userLogin = readln()
+        if (userLogin == userLoginRegistration) println(PASSWORD_REQUEST_1)
+        else println("Неверный логин.")
+    } while (userLogin != userLoginRegistration)
+
+    do {
+        val userPassword = readln()
+        authorization(userPassword, userPasswordRegistration)
+    } while (userPassword != userPasswordRegistration)
+
+    do {
+        val code = (1000..9999).random().toString()
+        println("Ваш код авторизации: $code")
+        println("Введите код:")
+        val userAnswer = readln()
+        if (userAnswer != code) println("Неверный код.")
+    } while (userAnswer != code)
+
+    println("Добро пожаловать!")
+
+}
+
+const val LOGIN_REQUEST_1 = "Введите логин:"
+const val PASSWORD_REQUEST_1 = "Введите пароль:"
+const val LOGIN_LENGHT = 4
