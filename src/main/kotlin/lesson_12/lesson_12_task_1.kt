f7182d000bb8daaa5c2760f5672727b4ca0e888f
Vadim <fullmetallboss@gmail.com>
2023-10-20 19:32:13 +0300

    KS-12-1

---
+++ src/main/kotlin/lesson_12/lesson_12_task_1.kt
@@ -0,0 +1,23 @@
+package lesson_12
+
+fun main() {
+
+    val weatherToday = DailyWeather()
+    weatherToday.dailyTemperature = 29
+    weatherToday.nightTemperature = 20
+    weatherToday.hasPrecipitationDuringTheDay = true
+    weatherToday.printDailyWeather()
+}
+
+class DailyWeather {
+
+    var dailyTemperature = 25
+    var nightTemperature = 18
+    var hasPrecipitationDuringTheDay = false
+
+    fun printDailyWeather (){
+        println("Информация о погоде:\n- дневная температура $dailyTemperature °C" +
+                "\n- ночная температура $nightTemperature °C" +
+                "\n- наличие осадков в течение суток: $hasPrecipitationDuringTheDay")
+    }
+}
