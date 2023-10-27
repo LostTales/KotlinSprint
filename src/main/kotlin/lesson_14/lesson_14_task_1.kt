ea265e642c7ccbc39d86aee2084e406fcc9b7cbf
Vadim <fullmetallboss@gmail.com>
2023-10-27 20:10:39 +0300

    KS-14-1

---
+++ src/main/kotlin/lesson_14/lesson_14_task_1.kt
@@ -0,0 +1,52 @@
+package lesson_14
+
+fun main() {
+
+    val liner = LinerShip()
+    liner.showShipInformation()
+
+    val cargoShip = CargoShip()
+    cargoShip.showShipInformation()
+
+    val icebreaker = IcebreakerShip()
+    icebreaker.showShipInformation()
+
+}
+
+open class LinerShip(
+    val name: String = "Лайнер",
+    val speed: Int = 25,
+    val numberOfPassengers: Int = 3000,
+    val shipTonnage: Int = 15_000,
+) {
+    open fun showShipInformation() {
+        println(
+            """
+            $name
+            $speed
+            $numberOfPassengers
+            $shipTonnage
+        """.trimIndent()
+        )
+    }
+}
+
+class CargoShip(
+    name: String = "Грузовой корабль",
+    speed: Int = 20,
+    numberOfPassengers: Int = 50,
+    shipTonnage: Int = 20_000,
+) : LinerShip(name, speed, numberOfPassengers, shipTonnage)
+
+class IcebreakerShip(
+    name: String = "Ледокол",
+    speed: Int = 18,
+    numberOfPassengers: Int = 150,
+    shipTonnage: Int = 10_000,
+    val isCanCrackIce: Boolean = true,
+) : LinerShip(name, speed, numberOfPassengers, shipTonnage) {
+    override fun showShipInformation() {
+        super.showShipInformation()
+        println(isCanCrackIce)
+    }
+}
