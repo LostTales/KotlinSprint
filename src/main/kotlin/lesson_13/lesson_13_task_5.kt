afdd6fa256016c6e4e1e024a4be29242b85952d2
Vadim <fullmetallboss@gmail.com>
2023-10-27 12:38:50 +0300

    KS-13-5

---
+++ src/main/kotlin/lesson_13/lesson_13_task_5.kt
@@ -0,0 +1,43 @@
+package lesson_13
+
+fun main() {
+
+    val contactList = mutableListOf<SubscriberData5>()
+
+    do {
+
+        println("Введите имя:")
+        val userName = readln()
+        if (userName.lowercase() == STOP2) break
+
+        println("Введите номер телефона:")
+        val userPhoneNumber = readln().toLong()
+        if (userPhoneNumber == null) {
+            println("Не указан номер телефона")
+            continue
+        }
+        if (userPhoneNumber.toString().lowercase() == STOP2) break
+
+        println("Введите название компании или пропустите этот шаг нажав Enter:")
+        val userCompany = readln()
+        if (userCompany.lowercase() == STOP2) break
+
+        val userData = SubscriberData5(userName, userPhoneNumber, userCompany)
+        contactList.add(userData)
+    } while (true)
+
+    contactList.forEach { println(it.printSubscriberData5()) }
+
+}
+
+class SubscriberData5(
+    var name: String?,
+    var phoneNumber: Comparable<*>,
+    var company: String? = null,
+) {
+    fun printSubscriberData5() {
+        println("$name\n$phoneNumber\n$company")
+    }
+}
+
+const val STOP2 = "стоп"
