18424e9442d3073c55b805322f31c744dd956466
Vadim <fullmetallboss@gmail.com>
2023-10-28 20:32:04 +0300

    KS-14-2 open fun

--- src/main/kotlin/lesson_14/lesson_14_task_2.kt
+++ src/main/kotlin/lesson_14/lesson_14_task_2.kt
@@ -4,20 +4,20 @@ fun main() {
 
     val liner = LinerShip2()
     liner.showShipInformation2()
-    liner.extendHorizontalLadder()
+    liner.activateShipLoading()
     println()
     val cargoShip = CargoShip2()
     cargoShip.showShipInformation2()
-    cargoShip.activateLoadingCrane()
+    cargoShip.activateShipLoading()
     println()
     val icebreaker = IcebreakerShip2()
     icebreaker.showShipInformation2()
-    icebreaker.openGate()
+    icebreaker.activateShipLoading()
 
 }
 
 open class LinerShip2(
-    val name: String = "Лайнер",
+    val name: String = "\"Лайнер\"",
     val speed: Int = 25,
     val numberOfPassengers: Int = 3000,
     val shipTonnage: Int = 15_000,
@@ -33,24 +33,25 @@ open class LinerShip2(
         )
     }
 
-    fun extendHorizontalLadder() {
+    open fun activateShipLoading() {
         println("На корабле $name выдвинут горизонтальный трап.")
     }
 }
 
 class CargoShip2(
-    name: String = "Грузовой корабль",
+    name: String = "\"Грузовой корабль\"",
     speed: Int = 20,
     numberOfPassengers: Int = 50,
     shipTonnage: Int = 20_000,
 ) : LinerShip2(name, speed, numberOfPassengers, shipTonnage) {
-    fun activateLoadingCrane() {
+
+    override fun activateShipLoading() {
         println("На корабле $name активирован погрузочный кран.")
     }
 }
 
 class IcebreakerShip2(
-    name: String = "Ледокол",
+    name: String = "\"Ледокол\"",
     speed: Int = 18,
     numberOfPassengers: Int = 150,
     shipTonnage: Int = 10_000,
@@ -61,7 +62,8 @@ class IcebreakerShip2(
         println("Возможность колоть лед: $isCanCrackIce")
     }
 
-    fun openGate() {
+    override fun activateShipLoading() {
         println("На корабле $name открыты ворота со стороны кормы.")
     }
+
 }
