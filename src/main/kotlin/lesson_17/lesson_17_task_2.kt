6f2284028d2e8be4112bf395cbb4ced8e6d80088
Vadim <fullmetallboss@gmail.com>
2023-11-07 19:05:14 +0300

    KS-17-2 v1.1

--- src/main/kotlin/lesson_17/lesson_17_task_2.kt
+++ src/main/kotlin/lesson_17/lesson_17_task_2.kt
@@ -1,18 +1,23 @@
 package lesson_17
 
+
 fun main() {
 
-    val ship = NewShip()
+    val ship = NewShip("Avenger", 50, "Nassau")
     ship.name = "Revenge"
+    println(ship.name)
 }
 
-class NewShip {
-    var name: String = "Avenger"
+class NewShip(
+
+    _name: String,
+    val averageSpeed: Int,
+    var homePort: String,
+) {
+
+    var name = _name
         set(value) {
             if (value != field) println("Имя корабля $field неизменно.")
-            field = value
+            field = name
         }
-
-    var averageSpeed: Int = 50
-    var homePort: String = "Nassau"
 }
