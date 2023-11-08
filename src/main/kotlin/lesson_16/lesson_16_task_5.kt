b80e4c1d738e807bfe59fb51b4065eac7261963e
Vadim <fullmetallboss@gmail.com>
2023-11-08 22:37:26 +0300

    KS-16-5 fun heal ??

--- src/main/kotlin/lesson_16/lesson_16_task_5.kt
+++ src/main/kotlin/lesson_16/lesson_16_task_5.kt
@@ -3,7 +3,7 @@ package lesson_16
 fun main() {
     println("Игра началась!")
 
-    val player1 = Player("Hero", 100, 1)
+    val player1 = Player("Hero", 3, 1)
 
     do {
         println("Игрок ${player1.name} сражается с противником. Здоровье ${player1.showHeals()}")
@@ -12,12 +12,12 @@ fun main() {
             player1.takeDamage()
             println("Игрок ${player1.name} получил урон. Здоровье ${player1.showHeals()}")
         } else {
-            player1.healing()
+            player1.heal()
             println("Игрок ${player1.name} выпил лечебное зелье. Здоровье ${player1.showHeals()}")
         }
     } while (player1.showHeals() > ZERO)
 
-    player1.getHealthIsOver(player1)
+    player1.getLoseAllHealth()
 
     println("Игрок погиб.\nИгра закончена.")
 }
@@ -34,20 +34,20 @@ class Player(
         health -= NUMBER_1
     }
 
-    fun healing() {
-        if (health > ZERO) health += NUMBER_1
+    fun heal() {
+        if (health in NUMBER_1.. health) health += NUMBER_1
     }
 
-    private fun healthIsOver() {
+    private fun loseAllHealth() {
         forceOfBlow = ZERO
         health = ZERO
     }
 
-    fun getHealthIsOver(player: Player) {
-        healthIsOver()
+    fun getLoseAllHealth() {
+        loseAllHealth()
     }
 }
 
-const val ZERO = 0
-const val NUMBER_1 = 1
-const val NUMBER_2 = 2
+private const val ZERO = 0
+private const val NUMBER_1 = 1
+private const val NUMBER_2 = 2
