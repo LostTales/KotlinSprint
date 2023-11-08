a953485c4f142f435c5e0c71c47bc4e64cb3e6cc
Vadim <fullmetallboss@gmail.com>
2023-11-08 23:43:06 +0300

    KS-17-4 value in if

--- src/main/kotlin/lesson_17/lesson_17_task_4.kt
+++ src/main/kotlin/lesson_17/lesson_17_task_4.kt
@@ -3,29 +3,32 @@ package lesson_17
 fun main() {
 
     val newPackage = NewPackage(1, "Boston")
+    println("${newPackage.location} ${newPackage.counter}")
     newPackage.location = "Detroit"
-    println(newPackage.counter)
+    println("${newPackage.location} ${newPackage.counter}")
     newPackage.location = "Chicago"
-    println(newPackage.counter)
+    println("${newPackage.location} ${newPackage.counter}")
     newPackage.location = "Chicago"
-    println(newPackage.counter)
+    println("${newPackage.location} ${newPackage.counter}")
     newPackage.location = "Dallas"
-    println(newPackage.counter)
+    println("${newPackage.location} ${newPackage.counter}")
     println()
     val newPackage2 = NewPackage(2, "Seattle")
+    println("${newPackage2.location} ${newPackage2.counter}")
     newPackage2.location = "Calgary"
-    println(newPackage2.counter)
+    println("${newPackage2.location} ${newPackage2.counter}")
 }
 
 class NewPackage(
-
     val number: Int,
     _location: String,
 ) {
     var location = _location
         set(value) {
-            if (value != field) ++counter
-            field = value
+            if (value != field) {
+                ++counter
+                field = value
+            }
         }
 
     var counter: Int = 0
