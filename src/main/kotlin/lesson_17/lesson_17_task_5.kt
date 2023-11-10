2aa588a3faf1b4aa8a9a6e59a096928309f4275c
Vadim <fullmetallboss@gmail.com>
2023-11-10 18:38:26 +0300

    KS-17-5 hash,SMS maybe time limit

--- src/main/kotlin/lesson_17/lesson_17_task_5.kt
+++ src/main/kotlin/lesson_17/lesson_17_task_5.kt
@@ -1,5 +1,8 @@
 package lesson_17
 
+import java.security.MessageDigest
+import java.util.*
+
 fun main() {
 
     val user1 = User17("user1", "4321k")
@@ -30,6 +33,24 @@ class User17(
             return hiddenPassword
         }
         set(value) {
-            if (value != field) println("\"Вы не можете изменить пароль\"")
+            val md = MessageDigest.getInstance("SHA-256")
+            md.update(field.toByteArray())
+            val digest = md.digest()
+            val hashedPassword = Base64.getEncoder().encodeToString(digest)
+            if (value != hashedPassword) {
+                println("\"Вы не можете изменить пароль\"")
+                println(hashedPassword)
+            } else {
+                val code = (1000..9999).random().toString()
+                println("Ваш код авторизации: $code")
+                println("Введите код:")
+                val userAnswer = readln()
+                if (userAnswer != code) {
+                    println("Неверный код.")
+                    return
+                } else {
+                    println("Принято.")
+                }
+            }
         }
 }
