100508d2b747dbd305dc18aac7022341aa457554
Vadim <fullmetallboss@gmail.com>
2023-11-14 19:35:05 +0300

    KS-21-2 sum()

--- src/main/kotlin/lesson_21/lesson_21_task_2.kt
+++ src/main/kotlin/lesson_21/lesson_21_task_2.kt
@@ -4,18 +4,7 @@ package lesson_21
 fun main() {
 
     val listOfNumbers = listOf(7, 15, 10, 3, 22, 100)
-    listOfNumbers.evenNumbersSum()
+    println(listOfNumbers.evenNumbersSum())
 }
 
-fun List<Int>.evenNumbersSum(): Int {
-
-    var sumOfEvenNumbers = 0
-
-    this.forEach {
-        if (it % 2 == 0) sumOfEvenNumbers += it
-    }
-
-    println(sumOfEvenNumbers)
-
-    return sumOfEvenNumbers
-}
+fun List<Int>.evenNumbersSum() = this.filter { it -> (it % 2) == 0 }.sum()
