2148d5b1228d3cd3e97874ea67cbc2757e541b30
Vadim <fullmetallboss@gmail.com>
2023-11-15 10:36:04 +0300

    KS-22-2 transfer text

--- src/main/kotlin/lesson_22/lesson_22_task_2.kt
+++ src/main/kotlin/lesson_22/lesson_22_task_2.kt
@@ -5,9 +5,12 @@ fun main() {
     val regularBook1 = NewRegularBook("name1", "author1")
     val dataBook1 = NewDataBook("name1", "author1")
 
-    println(regularBook1) // TODO "стандартная реализация toString() вывела название с адресом/ссылкой объекта, для вывода свойств надо переопределить метод toString() или сделать data class
+    println(regularBook1) /* TODO "стандартная реализация toString() вывела название с адресом/ссылкой объекта,
+    для вывода свойств надо переопределить метод toString() или сделать data class */
 
-    println(dataBook1) // TODO toString() уже по умолчанию возвращает свойства, но мы также можем редактировать вывод - переопределить toString()
+    println(dataBook1) /* TODO toString() уже по умолчанию возвращает свойства,
+    но мы также можем редактировать вывод - переопределить toString() */
+
 }
 
 class NewRegularBook(
